33b7fa321ed445b2b6d3bdb28cc838d8fa784893
sanemat <o.gata.ken@gmail.com>
2015-10-23 22:34:10 +0900

    feat(canonical): add implementation

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+  * http://upa-pc.blogspot.jp/2015/03/javascript-canonical-url-get.html
+  * Copyright © 2012-2015 Dr.ウーパ
+  */
 module.exports = function () {
-  return 'http://example.com/canonical';
+  var links = document.getElementsByTagName('link');
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].rel && links[i].rel.toLowerCase() === 'canonical') {
+      return links[i].href;
+    }
+  }
+  return null;
 };
