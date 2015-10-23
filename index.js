bf1bf3ff39f7f9d2f8694c01709809e0673ca118
sanemat <o.gata.ken@gmail.com>
2015-10-23 22:37:04 +0900

    docs(app): add document

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 /**
+  * @license
   * http://upa-pc.blogspot.jp/2015/03/javascript-canonical-url-get.html
   * Copyright © 2012-2015 Dr.ウーパ
+  *
+  * @return {?string} canonical url.
   */
 module.exports = function () {
   var links = document.getElementsByTagName('link');
