d17c415568a5d22d5f1521c8cdf09acdf1648e02
sanemat <o.gata.ken@gmail.com>
2015-10-24 10:54:38 +0900

    refactor(app): call length once

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
   */
 module.exports = function () {
   var links = document.getElementsByTagName('link');
-  for (var i = 0; i < links.length; i++) {
+  var linksLength = links.length;
+  for (var i = 0; i < linksLength; i++) {
     if (links[i].rel && links[i].rel.toLowerCase() === 'canonical') {
       return links[i].href;
     }
